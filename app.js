231ac16b463be4514493b01243ed549b639885e9
kristenoom <kris.a.oom@gmail.com>
2021-09-02 20:07:22 -0400

    app.js change

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+require('dotenv').config();
 let express = require('express');
 let app = express()
 let sequelize = require('./db');
 
-let comix = require('./controllers/comic-controller')
-let user = require('./controllers/user-controller')
+const comix = require('./controllers/comic-controller')
+const user = require('./controllers/user-controller')
 
 sequelize.sync()
 // sequlize.sync({force: true})
 
+app.use(express.json());
+app.use(require("./middleware/headers"));
+
 app.use('/user', user);
 
-app.use('/comix', comix)
+app.use('/comic', comix)
 
 app.listen(3000, function(){
     console.log('Exceslior');

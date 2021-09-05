c9f0bed0555726074215bdf8ecb36db84115bceb
RaheemMalik317 <82741663+RaheemMalik317@users.noreply.github.com>
2021-09-05 14:29:30 -0400

    register token update

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,19 +1,28 @@
 const router = require('express').Router();
 const User = require('../db').import('../models/user');
-//const jwt = require('jsonwebtoken');
-//const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
 router.post('/register', function (req, res) {
 
 
     User.create({
-      username: req.body.user.email,
-      password: req.body.user.password
+      username: req.body.user.username,
+      password: bcrypt.hashSync(req.body.user.password, 13)
     })
     .then (
-        res.send("This is our user endpoint!")
+      function createSuccess(user) {
+        let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
+        res.status(200).json({
+          user: user,
+          message: 'Excelsior!',
+          sessionToken: token
+        })
+      }
+       
     )
+    .catch(err => res.status(500).json({error: err}))
 })
 
 
